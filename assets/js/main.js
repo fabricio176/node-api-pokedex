49550188ba9a3419124e63c2da5c771ce226f03d
Fabrício Taveira <fabriciotaveira176@gmail.com>
2023-10-09 18:30:38 -0300

    Sixth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,35 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
-    
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="assets/images/svg/1.svg" alt="${pokemon.name}">
+        </div>
+
+    </li>`
+}
+
+const pokemonList = document.getElementById('pokemons');
+
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
+})
+
+
+
+
+
 
 
 
